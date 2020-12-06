2675ab9a92b2d44c99516614ee2fd0375a362c7b
EXEcutorXS <73341778+EXEcutorXS@users.noreply.github.com>
2020-12-06 11:14:57 +0400

    Initial commit

---
+++ ws2812.c
@@ -0,0 +1,67 @@
+#include "ws2812.h"
+extern TIM_HandleTypeDef htim2;
+
+uint16_t wsBuffer[ARRAY_LEN];
+color_t cBuffer[LED_COUNT];
+
+void wsSetColor(uint8_t num, uint8_t red, uint8_t green, uint8_t blue)
+{
+cBuffer[num].red=red;
+cBuffer[num].green=green;
+cBuffer[num].blue=blue;
+}
+
+void wsDoubleBright(uint8_t num)
+{
+	cBuffer[num].red*=2;
+	cBuffer[num].green*=2;
+	cBuffer[num].blue*=2;
+}
+
+void wsMultiply(uint8_t num, float k)
+{
+	cBuffer[num].red*=k;
+	cBuffer[num].green*=k;
+	cBuffer[num].blue*=k;
+}
+
+void swInit(void)
+{
+int i;
+for(i=0;i<ARRAY_LEN;i++)
+	wsBuffer[i]=0;
+}
+
+void wsPrepareArray()
+{
+	uint16_t i=0;
+	int16_t j=0;
+	uint32_t * adr=getBitBanded((uint32_t*)cBuffer, 0);
+
+	for(i=0;i<LED_COUNT*3;i++)
+	for(j=7;j>=0;j--)
+		if (*adr++)
+      wsBuffer[i*8+j]=65;
+		else
+	  wsBuffer[i*8+j]=26;
+}
+
+void wsRefreshLeds(TIM_HandleTypeDef * tim, uint32_t chan)
+{
+	HAL_TIM_PWM_Start_DMA(tim, chan, (uint32_t*)wsBuffer, ARRAY_LEN);
+}
+
+uint32_t * getBitBanded(uint32_t * adr,uint8_t bit)
+{
+return (uint32_t*)(((((uint32_t)adr)&0xFFFF))*32+bit*4+0x22000000);
+}
+
+void wsShiftLed(color_t newColor)
+{
+	uint8_t i;
+
+	for(i=LED_COUNT;i>0;i--)
+		cBuffer[i]=cBuffer[i-1];
+	cBuffer[0]=newColor;
+	wsPrepareArray();
+}
---
+++ ws2812.h
@@ -0,0 +1,40 @@
+#ifndef WS2812
+#define WS2812
+
+#include "main.h"
+
+typedef struct
+{
+uint8_t green;
+uint8_t red;
+uint8_t blue;
+}color_t;
+
+#define DELAY_LEN 200
+#define LED_COUNT (MAX_NODES + 1)
+#define ARRAY_LEN DELAY_LEN + LED_COUNT*24
+#define HIGH 65
+#define LOW 26
+
+#define RED 0x10,0,0
+#define GREEN 0,0x10,0
+#define BLUE 0,0,0x010
+#define YELLOW 0x10,0x10,0
+#define CYAN 0,0x10,0x10
+#define MAGENTA 0x10,0,0x10
+#define WHITE 0x10,0x10,0x10
+#define BLACK 0,0,0
+#define ORANGE 0x20,0x10,0x0
+#define PINK 0x20,0x0,0x10
+#define LGREEN 0x0,0x20,0x10
+
+
+void wsSetColor(uint8_t num, uint8_t red, uint8_t green, uint8_t blue);
+void swInit(void);
+void wsPrepareArray(void);
+void wsRefreshLeds(TIM_HandleTypeDef * tim, uint32_t chan);
+uint32_t * getBitBanded(uint32_t * adr,uint8_t bit);
+void wsShiftLed(color_t newColor);
+void wsDoubleBright(uint8_t num);
+void wsMultiply(uint8_t num, float k);
+#endif
